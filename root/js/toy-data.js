4fb6c6055dacf19cc940fa65077382761b11b078
knightsay <knightsay@hotmail.com>
2016-04-07 19:30:18 -0400

    upload map and radar chart

---
+++ root/js/toy-data.js
@@ -0,0 +1,32 @@
+var data = [
+    {
+        className: 'Faker', // optional can be used for styling
+        axes: [
+            {axis: "strength", value: 13, yOffset: 10},
+            {axis: "intelligence", value: 6},
+            {axis: "charisma", value: 5},
+            {axis: "dexterity", value: 9},
+            {axis: "luck", value: 2, xOffset: -20}
+        ]
+    },
+    {
+        className: 'Imp',
+        axes: [
+            {axis: "strength", value: 6},
+            {axis: "intelligence", value: 7},
+            {axis: "charisma", value: 10},
+            {axis: "dexterity", value: 13},
+            {axis: "luck", value: 9}
+        ]
+    },
+    {
+        className: 'Godv',
+        axes: [
+            {axis: "strength", value: 5},
+            {axis: "intelligence", value: 4},
+            {axis: "charisma", value: 10},
+            {axis: "dexterity", value: 11},
+            {axis: "luck", value: 8}
+        ]
+    }
+];
